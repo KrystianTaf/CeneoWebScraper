031190877b0519db5c6e82aadef3bf78e0454701
KrystianTaf <s221516@student.uek.krakow.pl>
2022-06-03 13:00:42 +0200

    2022-06-03 KT

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
+from fileinput import filename
 from app import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 import requests
 import json
 from bs4 import BeautifulSoup
+import os
+import pandas as pd
+import numpy as np
 
 def get_item(ancestor, selector, attribute=None, return_list=False):
     try:
@@ -33,36 +37,43 @@ selectors = {
 def index():
     return render_template("index.html.jinja")
 
-@app.route('/extract/<product_id>')
-def extract(product_id):
-    url = "https://www.ceneo.pl/"+product_id+"#tab=reviews"
-    all_opinions = []
-    while(url):
-        response = requests.get(url)
+@app.route('/extract', methods=["POST", "GET"])
+def extract():
+    if request.method == "POST":
+        product_id = request.form.get("product_id")
+        url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
+        all_opinions = []
+        while(url):
+            response = requests.get(url)
 
-        page = BeautifulSoup(response.text, "html.parser")
-        opinions = page.select("div.js_product-review")
-        for opinion in opinions:
-            single_opinion = {
-                key:get_item(opinion, *value)
-                    for key, value in selectors.items()
-            }
-            single_opinion["opinion_id"] = opinion["data-entry-id"]
-            all_opinions.append(single_opinion)
+            page = BeautifulSoup(response.text, "html.parser")
+            opinions = page.select("div.js_product-review")
+            for opinion in opinions:
+                single_opinion = {
+                    key:get_item(opinion, *value)
+                        for key, value in selectors.items()
+                }
+                single_opinion["opinion_id"] = opinion["data-entry-id"]
+                all_opinions.append(single_opinion)
 
-        try:
-            print(url)
-            url = "https://www.ceneo.pl"+page.select_one("a.pagination__next")["href"]
-        except TypeError:
-            url = None
+            try:
+                print(url)
+                url = "https://www.ceneo.pl"+page.select_one("a.pagination__next")["href"]
+            except TypeError:
+                url = None
+        if not os.path.exists("app/opinions"):
+            os.makedirs("app/opinions")
 
-    with open("opinions/"+product_id+".json", "w", encoding="UTF-8") as jf:
-        json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
-    return redirect(url_for('product', product_id=product_id))
+        with open(f"opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
+            json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
+        return redirect(url_for('product', product_id=product_id))
+    else:
+        return render_template("extract.html.jinja")
 
 @app.route('/products')
 def products():
-    pass
+    products = [filename.split(".")[0]]
+    return render_template("prodcuts.html.jinja", products=products)
 
 @app.route('/author')
 def author():
