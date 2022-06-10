3650ca22e67262036908f307fb3a04550eb422f2
KrystianTaf <s221516@student.uek.krakow.pl>
2022-06-10 13:00:45 +0200

    2022-06-10 KT

--- app/models/opinion.py
+++ app/models/opinion.py
@@ -1,3 +1,33 @@
+from app.parameters import selectors
+from app.utils import get_item
+
 class Opinion():
-    def __init__():
+    def __init__(self, author="", recommendation=None, stars=0, content="", useful=0, useless=0, publish_date=None, purchase_date=None, pros=[], cons=[], opinion_id=""):
+        self.author = author
+        self.recommendation = recommendation
+        self.stars = stars
+        self.content = content
+        self.useful = useful
+        self.useless = useless
+        self.publish_date = publish_date
+        self.purchase_date = purchase_date
+        self.pros = pros
+        self.cons = cons
+        self.opinion_id = opinion_id
+        return self
+    
+    def extract_opinon(self, opinion):
+        for key, value in selectors:
+            setattr(self, key, get_item(opinion, *value))
+            self.opinion_id = opinion["data-entry-id"]
+        
+        return self 
+
+    def __str__(self) -> str:
+        pass
+
+    def __repr__(self) -> str:
+        pass
+
+    def to_dict(self) -> dict:
         pass
